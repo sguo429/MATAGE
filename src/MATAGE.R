9aa51f6f0b193ee3f6d4579f6b08270ee0bab315
sguo429 <148508089+sguo429@users.noreply.github.com>
2025-08-19 15:51:40 -0500

    Update MATAGE.R

--- src/MATAGE.R
+++ src/MATAGE.R
@@ -4,6 +4,7 @@ library(Matrix)
 library(GMMAT)
 library(CompQuadForm)
 
+# --- Argument Parsing ---
 args <- commandArgs(trailingOnly = TRUE)
 geno_infile <- args[1]
 start_row <- as.numeric(args[2])
@@ -15,176 +16,194 @@ geno_start_col <- as.numeric(args[6])
 # Load the null model
 load(null_model_RData)
 
-# Function for conducting the association tests
+# --- Main Association Function ---
 MATAGE <- function(obj, infile, outfile, start_row, num_rows, geno_start_col){
+  
+  # Part 1: IO
+  if (grepl("\\.txt$", infile)) {
+    cmd <- sprintf("head -n 1 %s && tail -n +%d %s | head -n %d", infile, start_row + 1, infile, num_rows)
+  } else if (grepl("\\.txt\\.gz$", infile)) {
     cmd <- sprintf("gunzip -c %s | { head -n 1; tail -n +%d | head -n %d; }", infile, start_row + 1, num_rows)
-    geno_data <- fread(cmd = cmd, header = TRUE, data.table = FALSE)
+  } else {
+    stop("The genotype file format is not supported.")
+  }
+
+  geno_data_full <- fread(cmd = cmd, header = TRUE, data.table = FALSE)
+  
+  # Part 2: Data Preparation
+  # Match sample IDs between genotype file and null model
+  geno_sample_ids <- colnames(geno_data_full)[geno_start_col:ncol(geno_data_full)]
+  obj_ids <- unique(obj$id_include)
+  common_ids <- geno_sample_ids[geno_sample_ids %in% obj_ids]
+  if (any(!obj_ids %in% common_ids)) {
+    warning("Some id_include in obj are missing in the sample IDs of the infile!")
+  }
+  
+  # Subset the null model objects to the common samples
+  idx <- match(common_ids, obj_ids)
+  scaled.residuals <- obj$scaled.residuals[idx]
+  Sigma_i <- obj$Sigma_i[idx, idx]
+  Sigma_iX <- obj$Sigma_iX[idx, , drop = FALSE]
+  
+  # Separate variant info from the genotype data
+  info_data <- geno_data_full[, 1:(geno_start_col - 1), drop = FALSE]
+  
+  # Convert all genotype columns to a numeric matrix
+  #geno_matrix <- as.matrix(geno_data_full[, c(common_ids)])
+  geno_matrix <- do.call(cbind, lapply(geno_data_full[, c(common_ids)], as.numeric))
+
+  # Clean up large intermediate objects
+  rm(geno_data_full, geno_sample_ids, obj_ids, common_ids, idx)
+
+  # Check if the model is Linear or Logistic
+  is_binary_model <- family$family == "binomial"
+  # Check if it is a linear regression model
+  is_linear_regression <- !is_binary_model & (length(obj$theta) == 1)
+
+  # Function to compute p-values for a single genotype
+  compute_p_values <- function(G, info_row) {
     
-    geno_sample_ids <- colnames(geno_data)[geno_start_col:ncol(geno_data)]
-    obj_ids <- unique(obj$id_include)
-    common_ids <- intersect(geno_sample_ids, obj_ids)
-    if (any(!obj_ids %in% common_ids)) {
-        warning("Some id_include in obj are missing in the sample IDs of the infile!")
+    # --- Part 3: Linear Test ---
+    # Handle missing values
+    if(any(is.na(G))){
+        idx <- !is.na(G)
+        G <- G[idx]
+        scaled.residuals <- scaled.residuals[idx]
+        Sigma_i <- Sigma_i[idx, idx]
+        Sigma_iX <- Sigma_iX[idx, , drop = FALSE]
     }
 
-    info_cols <- colnames(geno_data)[1:(geno_start_col - 1)]
-    geno_data <- geno_data[, c(info_cols, common_ids)]
+    # Summary data of genotype
+    mean_G <- mean(G)
+    var_G <- var(G)
+    n <- length(G)
+    G0 <- sort(unique(G))
+    r <- length(G0)
+    
+    if (r == 1){
+        return(list(ID = info_row$ID, n = n, knots = r, mean_G = mean_G, var_G = var_G, p_value_l = NA, p_value_nl = NA))
+    }
 
-    idx <- match(common_ids, obj_ids)
-    scaled.residuals <- obj$scaled.residuals[idx]
-    X <- obj$X[idx, , drop = FALSE]
-    Y <- obj$Y[idx]
-    Sigma_i <- obj$Sigma_i[idx, idx]
-    Sigma_iX <- obj$Sigma_iX[idx, , drop = FALSE]
+    H_i <-  Sigma_i * obj$theta[1]
+    S_G <- as.numeric(crossprod(G, scaled.residuals))
+    Gt_res <- S_G * obj$theta[1]
+    p <- length(obj$coefficients)
+    Gt_Hi_G <- crossprod(G, crossprod(H_i, G))
+    Gt_Hi_X <- crossprod(G, Sigma_iX * obj$theta[1])
+    Xt_Hi_X_i <- obj$cov / obj$theta[1]
+    Gt_Px_G_i <- 1 / as.numeric((Gt_Hi_G - tcrossprod(Gt_Hi_X, tcrossprod(Gt_Hi_X, Xt_Hi_X_i))))
+    beta_hat <- Gt_res * Gt_Px_G_i
+    dispersion <- ifelse(is_binary_model, 1, ((n - p) * obj$theta[1] - Gt_res^2 * Gt_Px_G_i) / (n - p - 1))
+    Var_beta_hat <- dispersion * Gt_Px_G_i
+    W <- (beta_hat)^2 / Var_beta_hat
+    p_value_l <- ifelse(is_linear_regression, pf(W, 1, n - p - 1, lower.tail = FALSE), pchisq(W, df = 1, lower.tail = FALSE))
 
-    rm(common_ids, obj_ids, geno_sample_ids, idx, info_cols)
+    # --- Part 4: Nonlinear Test ---
+    if (r == 2) {
+        return(list(ID = info_row$ID, n = n, knots = r, mean_G = mean_G, var_G = var_G, p_value_l = p_value_l, p_value_nl = NA))
+    }
 
-    # Function to compute p-values for a genotype
-    compute_p_values <- function(row) {
-        G <- as.vector(as.numeric(row[, geno_start_col:ncol(row)]))
+    if(!is_binary_model){
+        # Updating using the new residual variance
+        Sigma_i <-  H_i / dispersion
+        Sigma_iX <- Sigma_iX * obj$theta[1] / dispersion
+        S_G <- Gt_res / dispersion
+        scaled.residuals <- scaled.residuals * obj$theta[1] / dispersion
+        cov <- Xt_Hi_X_i * dispersion
+    } else {
+        cov <- obj$cov
+    }
 
-        # Missing values
-        if(any(is.na(G))){
-            idx <- !is.na(G)
-            G <- G[idx]
-            scaled.residuals <- scaled.residuals[idx]
-            Sigma_i <- Sigma_i[idx, idx]
-            Sigma_iX <- Sigma_iX[idx, , drop = FALSE]
-        }
+    # Generate matrix J
+    J <- sparseMatrix(i=1:n, j=match(G, G0), x=1)
 
-        # Summary data of genotype
-        mean_G <- mean(G)
-        var_G <- var(G)
-        n <- length(G)
-        G0 <- sort(unique(G))
-        r <- length(G0)
-        
-        # Return NA p_values if there is only 1 knot
-        if (r == 1){
-            return(list(ID = row$ID, n = n, knots = r, mean_G = mean_G, var_G = var_G, p_value_l = NA, p_value_nl = NA))
-        }
+    # Prepare to construct matrix Q and R
+    h <- diff(G0)
+    h1 <- h[c(1:(r-2))]
+    h2 <- h[c(2:(r-1))]
+    h1_inv <- 1 / h1
+    h2_inv <- 1 / h2
+    cols <- r - 2
 
-        # Wald test for the linear effect
-        H_i <-  Sigma_i * obj$theta[1]
-        S_G <- as.numeric(crossprod(G, scaled.residuals))
-        Gt_res <- S_G * obj$theta[1]
-        p <- length(obj$coefficients)
-        Gt_Hi_G <- crossprod(G, crossprod(H_i, G))
-        Gt_Hi_X <- crossprod(G, Sigma_iX * obj$theta[1])
-        Xt_Hi_X_i <- obj$cov / obj$theta[1]
-        Gt_Px_G_i <- 1 / as.numeric((Gt_Hi_G - tcrossprod(Gt_Hi_X, tcrossprod(Gt_Hi_X, Xt_Hi_X_i))))
-        beta_hat <- Gt_res * Gt_Px_G_i
-        res_var <- ((n - p) * obj$theta[1] - Gt_res^2 * Gt_Px_G_i) / (n - p - 1)
-        Var_beta_hat <- res_var * Gt_Px_G_i
-        W <- (beta_hat)^2 / Var_beta_hat
-        p_value_l <- pf(W, 1, n - p - 1, lower.tail = FALSE)
+    # Prepare to construct matrix Q
+    q1 <- sparseMatrix(
+        i = rep(1:cols, each = 2),
+        j = as.vector(sapply(1:cols, function(k) k:(k+1))),
+        x = rep(c(1, -1), times = cols),
+        dims = c(cols, r)
+    )
 
+    q2 <- sparseMatrix(
+        i = rep(1:cols, each = 2),
+        j = as.vector(sapply(1:cols, function(k) c(k+1, k+2))),
+        x = rep(c(-1, 1), times = cols),
+        dims = c(cols, r)
+    )
 
-        # Return NA non-linear p_values if there are only 2 knots
-        if (r == 2) {
-            return(list(ID = row$ID, n = n, knots = r, mean_G = mean_G, var_G = var_G, p_value_l = p_value_l, p_value_nl = NA))
-        }
+    # Construct matrix Q
+    Q_trans <- q1*h1_inv + q2*h2_inv
+    Q <- t(Q_trans)
 
-        # Update using the new residual variance
-        Sigma_i <-  H_i / res_var
-        Sigma_iX <- Sigma_iX * obj$theta[1] / res_var
-        S_G <- Gt_res / res_var
-        scaled.residuals <- scaled.residuals * obj$theta[1] / res_var
-        cov <- Xt_Hi_X_i * res_var
-
-        # Generate J matrix
-        J <- sparseMatrix(i=1:n, j=match(G, G0), x=1)
-
-        # Prepare to generate matrix Q and R
-        h <- diff(G0)
-        h1 <- h[c(1:(r-2))]
-        h2 <- h[c(2:(r-1))]
-        h1_inv <- 1 / h1
-        h2_inv <- 1 / h2
-        cols <- r - 2
-
-        # Prepare to generate matrix Q
-        q1 <- sparseMatrix(
-            i = rep(1:cols, each = 2),
-            j = as.vector(sapply(1:cols, function(k) k:(k+1))),
-            x = rep(c(1, -1), times = cols),
-            dims = c(cols, r)
+    # Prepare to construct matrix R
+    diag_vals <- rep(1/3, cols)
+    off_diag_vals <- rep(1/6, cols - 1)
+     
+    if (cols > 1){
+        r1 <- sparseMatrix(
+        i = c(1:cols, 2:cols),
+        j = c(1:cols, 1:(cols-1)),
+        x = c(diag_vals, off_diag_vals),
+        dims = c(cols, cols)
         )
 
-        q2 <- sparseMatrix(
-            i = rep(1:cols, each = 2),
-            j = as.vector(sapply(1:cols, function(k) c(k+1, k+2))),
-            x = rep(c(-1, 1), times = cols),
-            dims = c(cols, r)
-        )
-
-        # Generate matrix Q
-        Q_trans <- q1*h1_inv + q2*h2_inv
-        Q <- t(Q_trans)
-
-        # Prepare to generate matrix R
-        diag_vals <- rep(1/3, cols)
-        off_diag_vals <- rep(1/6, cols - 1)
-         
-        if (cols > 1){
-            r1 <- sparseMatrix(
-            i = c(1:cols, 2:cols),
-            j = c(1:cols, 1:(cols-1)),
+        r2 <- sparseMatrix(
+            i = c(1:cols, 1:(cols-1)),
+            j = c(1:cols, 2:cols),
             x = c(diag_vals, off_diag_vals),
             dims = c(cols, cols)
-            )
-
-            r2 <- sparseMatrix(
-                i = c(1:cols, 1:(cols-1)),
-                j = c(1:cols, 2:cols),
-                x = c(diag_vals, off_diag_vals),
-                dims = c(cols, cols)
-            )
-        } else {
-            r1 <- 1/3
-            r2 <- 1/3
-        }
-
-        # Generate matrix R
-        R <- r1*h1 + r2*h2
-
-        # Cholesky decomposition of R
-        C <- chol(R)
-
-        # Generate matrix B
-        B0 <- crossprod(Q_trans, tcrossprod(solve(crossprod(Q)), C))
-        B <- J %*% B0
-        S_B <- crossprod(B, scaled.residuals)
-
-        # Score test for the nonlinear effect
-        Jt_Sigma_iX <- crossprod(J, Sigma_iX)
-        Jt_Sigma_iJ <- crossprod(J, crossprod(Sigma_i, J))
-        JtPJ <- Jt_Sigma_iJ - tcrossprod(Jt_Sigma_iX, tcrossprod(Jt_Sigma_iX, cov))
-        JtPJ_B0 <- crossprod(JtPJ, B0)
-        GtPB <- crossprod(G0, JtPJ_B0)
-        GtPG_i <- solve(crossprod(G0, crossprod(JtPJ, G0)))
-        M <- crossprod(GtPG_i, GtPB)
-        S_B_hat <- S_B - crossprod(M, S_G)
-        V <- crossprod(B0, JtPJ_B0) - crossprod(M, GtPB)
-        p_value_nl <- .quad_pval(U = S_B_hat, V = V, method = "davies")
-
-        return(list(ID = row$ID, n = n, knots = r, mean_G = mean_G, var_G = var_G, p_value_l = p_value_l, p_value_nl = p_value_nl))
+        )
+    } else {
+        r1 <- 1/3
+        r2 <- 1/3
     }
 
-    # Compute test results for all the genotypes
-    results_list <- mclapply(1:nrow(geno_data), function(i) compute_p_values(geno_data[i, ]), mc.cores = 1)
+    # Construct matrix R
+    R <- r1*h1 + r2*h2
 
-    # Combine results into a data frame 
-    results <- rbindlist(results_list, use.names=TRUE, fill=TRUE)
+    # Cholesky decomposition of R
+    C <- chol(R)
 
-    # Calculate the joint p-value
-    results$p_value_joint <- apply(results[, c("p_value_l", "p_value_nl")], 1, fisher_pval)
+    # Generate matrix B
+    B0 <- crossprod(Q_trans, tcrossprod(solve(crossprod(Q)), C))
+    S_B <- crossprod(B0, crossprod(J, scaled.residuals))
 
-    # Save the results
-    fwrite(results, outfile, quote=FALSE, row.names=FALSE, col.names=TRUE, sep="\t", na="NA")
+    # Score test for the nonlinear effect
+    Jt_Sigma_iX <- crossprod(J, Sigma_iX)
+    Jt_Sigma_iJ <- crossprod(J, crossprod(Sigma_i, J))
+    JtPJ <- Jt_Sigma_iJ - tcrossprod(Jt_Sigma_iX, tcrossprod(Jt_Sigma_iX, cov))
+    JtPJ_B0 <- crossprod(JtPJ, B0)
+    GtPB <- crossprod(G0, JtPJ_B0)
+    GtPG_i <- solve(crossprod(G0, crossprod(JtPJ, G0)))
+    M <- crossprod(GtPG_i, GtPB)
+    S_B_hat <- S_B - crossprod(M, S_G)
+    V <- crossprod(B0, JtPJ_B0) - crossprod(M, GtPB)
+    p_value_nl <- .quad_pval(U = S_B_hat, V = V, method = "davies")
+    
+    return(list(ID = info_row$ID, n = n, knots = r, mean_G = mean_G, var_G = var_G, p_value_l = p_value_l, p_value_nl = p_value_nl))
+  }
+
+  # Loop over the rows of the genotype matrix
+  results_list <- mclapply(1:nrow(geno_matrix), function(i) {
+    compute_p_values(G = geno_matrix[i, ], info_row = info_data[i, ])
+  }, mc.cores = 1)
+  
+  # Combine and save results
+  results <- rbindlist(results_list, use.names=TRUE, fill=TRUE)
+  results$p_value_joint <- apply(results[, c("p_value_l", "p_value_nl")], 1, fisher_pval)
+  fwrite(results, outfile, quote=FALSE, row.names=FALSE, col.names=TRUE, sep="\t", na="NA")
 }
 
-
+# --- Helper Functions ---
 .quad_pval <- function(U, V, method = "davies") {
     Q <- sum(U^2)
     lambda <- eigen(V, only.values = TRUE, symmetric=TRUE)$values
@@ -267,5 +286,5 @@ fisher_pval <- function(p) {
   pchisq(-2*sum(log(p)), df = 2*length(p), lower.tail = FALSE)
 }
 
-# Conduct the association tests for selected genotypes
+# --- Run the Analysis ---
 MATAGE(null_model, geno_infile, outfile, start_row, num_rows, geno_start_col)
